85f3ee84dce60616c54c34929e7a0f4e9bf071ca
Kushal <kkanodia7@gmail.com>
2019-08-31 12:05:37 -0400

    Fixed clear command, cleaned up code

--- sketchpad.py
+++ sketchpad.py
@@ -1,3 +1,5 @@
+# Created by Kushal Kanodia on Jan 3, 2017
+
 from tkinter import Tk, Canvas
 from random import randint
 
@@ -8,32 +10,6 @@ canvas.pack()
 tool = 1
 color = "black"
 points = []
-
-canvas.create_line(80, 0, 80, 480)
-canvas.create_line(0, 80, 80, 80)
-canvas.create_line(0, 160, 80, 160)
-canvas.create_line(0, 240, 80, 240)
-canvas.create_line(0, 320, 80, 320)
-
-canvas.create_line(15, 65, 65, 15)
-canvas.create_line(15, 145, 45, 95, 65, 120)
-canvas.create_rectangle(15, 185, 65, 215)
-
-canvas.create_line(40, 270, 40, 290)
-canvas.create_line(30, 280, 50, 280)
-canvas.create_line(35, 275, 45, 285)
-canvas.create_line(35, 285, 45, 275)
-
-for r in range(300):
-    rpixelx = randint(40 - 20, 40 + 20)
-    rpixely = randint(360 - 20, 360 + 20)
-    if (rpixelx - 40) ** 2 + (rpixely - 360) ** 2 <= 20 ** 2:
-        canvas.create_line(rpixelx, rpixely, rpixelx + 1, rpixely + 1, fill=color)
-
-canvas.create_rectangle(0, 400, 40, 440, fill="black")
-canvas.create_rectangle(40, 400, 80, 440, fill="red")
-canvas.create_rectangle(0, 440, 40, 480, fill="green")
-canvas.create_rectangle(40, 440, 80, 480, fill="blue")
 
 
 def click(event):
@@ -107,14 +83,36 @@ def release(event):
 
 def clear(event):
     canvas.delete("all")
+    canvas.create_line(80, 0, 80, 480)
+    canvas.create_line(0, 80, 80, 80)
+    canvas.create_line(0, 160, 80, 160)
+    canvas.create_line(0, 240, 80, 240)
+    canvas.create_line(0, 320, 80, 320)
+
+    canvas.create_line(15, 65, 65, 15)
+    canvas.create_line(15, 145, 45, 95, 65, 120)
+    canvas.create_rectangle(15, 185, 65, 215)
+
+    canvas.create_line(40, 270, 40, 290)
+    canvas.create_line(30, 280, 50, 280)
+    canvas.create_line(35, 275, 45, 285)
+    canvas.create_line(35, 285, 45, 275)
+
+    for r in range(300):
+        rpixelx = randint(40 - 20, 40 + 20)
+        rpixely = randint(360 - 20, 360 + 20)
+        if (rpixelx - 40) ** 2 + (rpixely - 360) ** 2 <= 20 ** 2:
+            canvas.create_line(rpixelx, rpixely, rpixelx + 1, rpixely + 1, fill="black")
+
+    canvas.create_rectangle(0, 400, 40, 440, fill="black")
+    canvas.create_rectangle(40, 400, 80, 440, fill="red")
+    canvas.create_rectangle(0, 440, 40, 480, fill="green")
+    canvas.create_rectangle(40, 440, 80, 480, fill="blue")
 
 
+clear(0)
 root.bind("c", clear)
 root.bind("<Button-1>", click)
 root.bind("<B1-Motion>", drag)
 root.bind("<ButtonRelease-1>", release)
 root.mainloop()
-
-
-
-
